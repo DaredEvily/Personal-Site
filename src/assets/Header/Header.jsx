14403d1bc2d20249acf4320fc2544bdb2f4f1732
Ahmad G <ahmedjokar082@gmail.com>
2024-07-03 00:04:25 +0300

    edited

--- src/assets/Header/Header.jsx
+++ src/assets/Header/Header.jsx
@@ -3,7 +3,7 @@ export default()=>(
     <header>
     <div class="container">
       <nav>
-        <a href="#hero" class="logo">Ahmed.G</a>
+        <a href="#hero" class="logo">Home</a>
         <ul class="nav-links">
           <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
